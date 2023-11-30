22928906391d4de06b9ea8eac86076d33da3c05d
Hermenilsonsouza <your_github_email>
2023-11-30 09:45:47 -0300

    feat: pass slug at view post

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,11 +1,19 @@
+from blog.models import Post
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
-posts = list(range(1000))
+PER_PAGE = 9
 
 
 def index(request):
-    paginator = Paginator(posts, 9)
+    posts = (
+        Post
+        .objects
+        .filter(is_published=True)
+        .order_by('-pk')
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
@@ -19,10 +27,6 @@ def index(request):
 
 
 def page(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
     return render(
         request,
         'blog/pages/page.html',
@@ -32,11 +36,7 @@ def page(request):
     )
 
 
-def post(request):
-    paginator = Paginator(posts, 9)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
+def post(request, slug):
     return render(
         request,
         'blog/pages/post.html',
--- djangoapp/site_setup/context_processors.py
+++ djangoapp/site_setup/context_processors.py
@@ -3,8 +3,6 @@ from site_setup.models import SiteSetup
 def site_setup(request):
     setup = SiteSetup.objects.order_by('-id').first()
 
-    print(setup)
-
     return {
         'site_setup': setup
     }
